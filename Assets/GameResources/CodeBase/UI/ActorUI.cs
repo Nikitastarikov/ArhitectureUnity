bd69dab73754b2e443a7b3e8c1dcd06bc42cb097
Nikita <nikitka_starikov_99@mail.ru>
2022-11-06 01:25:24 +0700

    Enemy states and main logic.

---
+++ Assets/GameResources/CodeBase/UI/ActorUI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using CodeBase.Hero;
+using CodeBase.Logic;
+
+namespace CodeBase.UI
+{
+    public class ActorUI : MonoBehaviour
+    {
+        [SerializeField]
+        private HpBar _hpBar;
+
+        private IHealth _health;
+
+        public void Constructor(IHealth health)
+        {
+            _health = health;
+
+            _health.onHealthChanged += UpdateHpBar;
+        }
+
+        private void Start()
+        {
+            if (_health == null)
+            {
+                _health = GetComponent<IHealth>();
+
+                if (_health != null)
+                    Constructor(_health);
+            }
+        }
+
+        private void OnDestroy() => 
+            _health.onHealthChanged -= UpdateHpBar;
+
+        private void UpdateHpBar() => 
+            _hpBar.SetValue(_health.Current, _health.Max);
+    }
+}
